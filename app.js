8e79b0dfa65e118a279d26f2cf6c5ac0d23c041a
hriddhi <35338363+hriddhi@users.noreply.github.com>
2020-07-07 17:31:18 +0530

    Added basic authorization

--- app.js
+++ app.js
@@ -27,10 +27,43 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+//middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+function auth(req, res, next) {
+  console.log(req.headers);
+
+  var authHeader = req.headers.authorization;
+
+  if(!authHeader){
+    var err = new Error('You are not authenticated');
+
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401;
+    return next(err);
+  }
+
+  var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
+
+  var username = auth[0];
+  var password = auth[1];
+
+  if(username === 'admin' && password === 'password') {
+    next();
+  } else {
+    var err = new Error('You are not authenticated');
+
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401;
+    return next(err);
+  }
+}
+
+app.use(auth);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
