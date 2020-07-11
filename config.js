1f66a00f3671a5d080137b507f1002a1d80dd86e
hriddhi <35338363+hriddhi@users.noreply.github.com>
2020-07-11 18:02:14 +0530

    Added Facebook OAuth

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey' : '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '3311316828914359',
+        clientSecret: '6d1214015b02e7c2f3b822b91900c4fc'
+    }
 }
